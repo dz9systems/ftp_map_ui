42f1087690c621705794a98d0a75295c2c1f38e6
Isaiah Love <isaiahlove805@yahoo.com>
2022-09-29 14:44:05 -0700

    initial set up

---
+++ data.js
@@ -0,0 +1,69 @@
+const data = [
+  {
+    state: 'California',
+    law_passed: null,
+    law_proposed: 'proposed',
+    year: 2021,
+    title_of_law: 'AB 1076',
+    prison_pop:100000
+  },
+{
+  state: 'Illinois',
+  law_passed: null,
+  law_proposed: 'proposed',
+   year: 2021,
+   title_of_law: 'AB 2076',
+   prison_pop:150000
+},
+{
+  state: 'Louisiana',
+  law_passed: null,
+  law_proposed: 'proposed',
+   year: 2022,
+   title_of_law: 'AB 3076',
+   prison_pop:200000
+},
+{
+  state: 'Oregon',
+  law_passed: null,
+  law_proposed: 'proposed',
+   year: 2022,
+   title_of_law: 'AB 4076',
+   prison_pop:250000
+},
+{
+  state: 'Washington',
+  law_passed: null,
+  law_proposed: 'proposed',
+   year: 2022,
+   title_of_law: 'AB 5076',
+   prison_pop:300000
+},
+{
+  state: 'Minnesota',
+  law_passed: 'passed',
+  law_proposed: null,
+  year: 2019,
+  title_of_law: 'AB 6076',
+  prison_pop:350000
+},
+{
+  state: 'New York',
+  law_passed: 'passed',
+  law_proposed: null,
+  year: 2019,
+  title_of_law: 'AB 7076',
+  prison_pop:400000
+},
+]
+
+
+
+
+
+
+
+
+
+
+
